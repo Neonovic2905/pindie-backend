36c5efbd33e26bf23066568488b68ea4b9c9ef54
Neonovic2905 <neon7582@gmail.com>
2024-05-26 19:55:38 +0300

    update CORS for mgnix

--- middlewares/cors.js
+++ middlewares/cors.js
@@ -1,22 +1,14 @@
-const allowedCors = [
-    'https://practicum.yandex.ru',
-    'https://students-projects.ru',
-    'localhost:3000', 
-    'localhost:3001',
-    "http://localhost:3000"     
-  ]; 
-  function cors(req, res, next) {
-    const { origin } = req.headers;
-//    console.log(origin);
-    if (allowedCors.includes(origin)) { // Если это наш друг
-        res.header('Access-Control-Allow-Origin', origin);
- 
-    }
-    
-    next();
-} 
+const allowedCors = ["https://pindie-theme-8.nomoredomainswork.ru"];
 
+function cors(req, res, next) {
+  const { origin } = req.headers;
 
-module.exports = {
-    cors
-  } ;
+  if (allowedCors.includes(origin)) {
+    res.header("Access-Control-Allow-Origin", origin);
+  }
+  res.header("Access-Control-Allow-Methods", "GET,HEAD,PUT,PATCH,POST,DELETE");
+  res.header("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers, Authorization");
+  next();
+}
+
+module.exports = cors; 
